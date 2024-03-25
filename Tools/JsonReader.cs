e99a959189e8768512ed773a38b47e9cc3aa4570
Innokentiy945 <forjimmy945@Gmail.com>
2024-03-25 16:07:11 +0200

    Changes

---
+++ Tools/JsonReader.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GeoDetectorMvcApp.Tools;
+
+public class JsonReader
+{
+    public JArray ReadJsonByFilePath(string filePath)
+    {
+        using (StreamReader file = File.OpenText(filePath))
+        using (JsonTextReader reader = new(file))
+        {
+            JArray outPut = JToken.ReadFrom(reader) as JArray;
+            return outPut;
+        }
+    }
+}
+
